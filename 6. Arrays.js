675ddd8a555b1bc383d054e6e607e98dfe1ba8d4
Irzam Tahir <irzamtahir4@gmail.com>
2023-08-23 18:25:53 +0500

    Done till 6.13

--- 6. Arrays.js
+++ 6. Arrays.js
@@ -36,8 +36,8 @@
 // console.log(num);
 
 //7 - Emptying an Array
-let num = [1,2,3,4];
-let another = num;
+// let num = [1,2,3,4];
+// let another = num;
 
 //Sol 1
 // num = []
@@ -54,5 +54,65 @@ let another = num;
 // }
 
 
-console.log(num);
-console.log(another);
+// console.log(num);
+// console.log(another);
+
+//8 Combing and Slicing Arrays
+// const first = [1,2,3];
+// const second = [4,5,6];
+
+// const combine = first.concat(second);
+// console.log(combine);
+// const slice = combine.slice(2,4)
+// console.log(slice);
+
+// a = { x: 10 };
+// b = a;
+
+// console.log(a.x = 20);
+// console.log(b);
+
+// 9 - Spread Operator
+// const combined = [...first, 'a' ,...second, 'b'];
+// console.log(combined);
+
+// const copy = [...combined]
+// console.log(copy);
+
+//10 - iterating an array
+// const num = [1,2,3] 
+
+// for (let nums of num)
+//     console.log(nums);
+
+// num.forEach((number, index) => console.log(index, number));
+
+// for (let i = 0; i < num.length; i++) 
+//     console.log(num[i]);
+
+//11 - Joining Arrays
+// const numbers = [1,2,3];
+// const join = numbers.join(" ");  // Array to String
+// console.log(join);
+
+// const message = 'This is my first message';
+// const parts = message.split(' '); // String to Array
+// console.log(parts);
+
+// const combined = parts.join(' ');
+// console.log(combined);
+
+//12 - Sorting Arrays
+// const courses = [
+//     { id: 1, name: 'Node:js'},
+//     { id: 2, name: 'JavaScript'}
+// ];
+
+// courses.sort(function(a, b){
+//     const
+
+//     if (a.name < b.name) return -1;
+//     if (a.name > b.name) return 1;
+//     return 0;
+// })
+// console.log(courses);
