5fb422f8f7ee34d7052feb224d85278c4e6c4016
Irzam Tahir <irzamtahir4@gmail.com>
2023-09-02 03:12:18 +0500

    Started OOP

---
+++ 2. Ultimate JavaScript Series Part 2 - Advanced Topics/1. Getting Started.js
@@ -0,0 +1,15 @@
+// 1 - What is OOP?
+/*********************************************************************************************************** */
+/*
+A programming paradigm centered around objects rather than functions  
+*/
+/*********************************************************************************************************** */
+
+// 2 - Four Pillars of OOP
+/*********************************************************************************************************** */
+/*
+1. Encapsulation --> we group related variables and functions together and this way we can reduce complexity and increases reusability
+2. Abstraction --> we hide the details and complexity and show only the essentials and isolate the impact of changes in the code
+3. Inheritance --> we can eliminate the redundant code
+4. Polymorphism --> Refactor ugly switch/case statements
+*/ 
